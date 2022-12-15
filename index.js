99da62679bd389238dcd02bdf96d24e73d494bfc
sravangundlur <109715897+sravangundlur@users.noreply.github.com>
2022-12-15 09:38:09 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('{ "response": "Hello  Assert" }');
+});
+
+app.get('/will', function (req, res) {
+    res.send('{ "response": "this is sravan" }');
+});
+app.get('/ready', function (req, res) {
+    res.send('{ "response": " Great!, To be part of assret team" }');
+});
+app.listen(process.env.PORT || 3000);
+module.exports = app;
